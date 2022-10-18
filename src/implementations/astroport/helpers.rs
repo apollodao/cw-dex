62f83ccb67fb3e32ad956102001dda4b57ead00d
Bobby <bobby@apollo.farm>
2022-10-18 19:24:06 +0200

    Add withdraw_liquidity
    Signed-off-by: Bobby <bobby@apollo.farm>

--- src/implementations/astroport/helpers.rs
+++ src/implementations/astroport/helpers.rs
@@ -2,6 +2,8 @@ use astroport_core::asset::{Asset as AstroAsset, AssetInfo as AstroAssetInfo};
 use cosmwasm_std::{StdError, StdResult};
 use cw_asset::{Asset, AssetInfo, AssetList};
 
+use crate::CwDexError;
+
 pub(crate) struct AstroAssetList(pub(crate) Vec<AstroAsset>);
 
 impl From<AstroAssetList> for Vec<AstroAsset> {
@@ -21,20 +23,7 @@ impl TryFrom<AssetList> for AstroAssetList {
     fn try_from(list: AssetList) -> StdResult<Self> {
         Ok(Self(
             list.into_iter()
-                .map(|a| {
-                    Ok(AstroAsset {
-                        info: match &a.info {
-                            AssetInfo::Native(denom) => Ok(AstroAssetInfo::NativeToken {
-                                denom: denom.to_string(),
-                            }),
-                            AssetInfo::Cw20(contract_addr) => Ok(AstroAssetInfo::Token {
-                                contract_addr: contract_addr.clone(),
-                            }),
-                            _ => Err(StdError::generic_err("Invalid asset info")),
-                        }?,
-                        amount: a.amount,
-                    })
-                })
+                .map(|a| cw_asset_to_astro_asset(a))
                 .collect::<StdResult<Vec<AstroAsset>>>()?,
         ))
     }
@@ -58,4 +47,19 @@ impl From<AstroAssetList> for AssetList {
             .collect::<Vec<Asset>>()
             .into()
     }
+}
+
+pub(crate) fn cw_asset_to_astro_asset(asset: &Asset) -> StdResult<AstroAsset> {
+    Ok(AstroAsset {
+        info: match &asset.info {
+            AssetInfo::Native(denom) => Ok(AstroAssetInfo::NativeToken {
+                denom: denom.to_string(),
+            }),
+            AssetInfo::Cw20(contract_addr) => Ok(AstroAssetInfo::Token {
+                contract_addr: contract_addr.clone(),
+            }),
+            _ => Err(StdError::generic_err("Invalid asset info")),
+        }?,
+        amount: asset.amount,
+    })
 }
