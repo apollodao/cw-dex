f02245b8b34c57071da65b35beba513149e98919
Bobby <bobby@apollo.farm>
2022-10-24 18:32:36 +0200

    Add simulations
    Signed-off-by: Bobby <bobby@apollo.farm>

--- src/implementations/astroport/astroport.rs
+++ src/implementations/astroport/astroport.rs
@@ -2,16 +2,20 @@ use std::sync::mpsc::RecvError;
 
 use astroport_core::factory::PairType;
 use astroport_core::querier::query_supply;
+use astroport_core::U256;
 use cosmwasm_std::{
-    to_binary, Addr, Coin, CosmosMsg, Decimal, Querier, QuerierWrapper, Response, StdResult,
-    WasmMsg,
+    to_binary, Addr, Coin, CosmosMsg, Decimal, Querier, QuerierWrapper, QueryRequest, Response,
+    StdError, StdResult, WasmMsg, WasmQuery,
 };
 use cosmwasm_std::{Deps, Event, Uint128};
 use cw20::{Cw20ExecuteMsg, Cw20ReceiveMsg};
 use cw_asset::{Asset, AssetInfo, AssetInfoBase, AssetList, AssetListBase};
 
 use astroport_core::asset::{Asset as AstroAsset, AssetInfo as AstroAssetInfo};
-use astroport_core::pair::{Cw20HookMsg, ExecuteMsg as PairExecMsg};
+use astroport_core::pair::{
+    ConfigResponse, Cw20HookMsg, ExecuteMsg as PairExecMsg, PoolResponse, QueryMsg,
+    SimulationResponse,
+};
 
 use crate::pool::Pool;
 use crate::CwDexError;
@@ -38,15 +42,15 @@ impl AstroportXykPool {
 
     fn swap_native_msg(
         &self,
-        offer_asset: Asset,
-        ask_asset_info: AssetInfo,
+        offer_asset: &Asset,
+        ask_asset_info: &AssetInfo,
         belief_price: Decimal,
         recipient: Addr,
     ) -> Result<Response, CwDexError> {
-        let asset = cw_asset_to_astro_asset(&offer_asset)?;
+        let asset = cw_asset_to_astro_asset(offer_asset)?;
         let msg = PairExecMsg::Swap {
             offer_asset: asset,
-            ask_asset_info: Some(cw_asset_info_to_astro_asset_info(&ask_asset_info)?),
+            ask_asset_info: Some(cw_asset_info_to_astro_asset_info(ask_asset_info)?),
             belief_price: Some(belief_price),
             max_spread: Some(Decimal::zero()),
             to: Some(recipient.into_string()),
@@ -61,19 +65,19 @@ impl AstroportXykPool {
 
     fn swap_cw20_msg(
         &self,
-        offer_asset: Asset,
-        ask_asset_info: AssetInfo,
+        offer_asset: &Asset,
+        ask_asset_info: &AssetInfo,
         belief_price: Decimal,
         recipient: Addr,
     ) -> Result<Response, CwDexError> {
-        if let AssetInfoBase::Cw20(token_addr) = offer_asset.info {
+        if let AssetInfoBase::Cw20(token_addr) = offer_asset.to_owned().info {
             let swap = CosmosMsg::Wasm(WasmMsg::Execute {
                 contract_addr: token_addr.to_string(),
                 msg: to_binary(&Cw20ExecuteMsg::Send {
                     contract: self.contract_addr.to_string(),
                     amount: Uint128::zero(), // Should this be `offer_asset.amount`?
                     msg: to_binary(&Cw20HookMsg::Swap {
-                        ask_asset_info: Some(cw_asset_info_to_astro_asset_info(&ask_asset_info)?),
+                        ask_asset_info: Some(cw_asset_info_to_astro_asset_info(ask_asset_info)?),
                         belief_price: Some(belief_price),
                         max_spread: Some(Decimal::zero()),
                         to: Some(recipient.into_string()),
@@ -84,7 +88,7 @@ impl AstroportXykPool {
             Ok(Response::new().add_message(swap))
         } else {
             Err(CwDexError::InvalidInAsset {
-                a: offer_asset,
+                a: offer_asset.to_owned(),
             })
         }
     }
@@ -166,10 +170,10 @@ impl Pool for AstroportXykPool {
         let belief_price = Decimal::from_ratio(minimum_out_amount, 1u128);
         let response = match offer_asset.info {
             AssetInfo::Native(_) => {
-                self.swap_native_msg(offer_asset.clone(), ask_asset_info, belief_price, recipient)
+                self.swap_native_msg(&offer_asset, &ask_asset_info, belief_price, recipient)
             }
             AssetInfo::Cw20(_) => {
-                self.swap_cw20_msg(offer_asset.clone(), ask_asset_info, belief_price, recipient)
+                self.swap_cw20_msg(&offer_asset, &ask_asset_info, belief_price, recipient)
             }
             _ => Err(CwDexError::InvalidInAsset {
                 a: offer_asset.clone(),
@@ -184,15 +188,91 @@ impl Pool for AstroportXykPool {
     }
 
     fn get_pool_liquidity(&self, deps: Deps) -> Result<AssetList, CwDexError> {
-        todo!()
+        let query_msg = QueryMsg::Pool {};
+        let wasm_query = WasmQuery::Smart {
+            contract_addr: self.contract_addr.to_string(),
+            msg: to_binary(&query_msg)?,
+        };
+        let query_request = QueryRequest::Wasm(wasm_query);
+        let resp = deps.querier.query::<PoolResponse>(&query_request)?;
+        Ok(AssetList::from(AstroAssetList(resp.assets)))
     }
 
     fn simulate_provide_liquidity(
         &self,
         deps: Deps,
-        asset: AssetList,
+        assets: AssetList,
     ) -> Result<Asset, CwDexError> {
-        todo!()
+        let astro_assets: AstroAssetList = assets.try_into()?;
+        let mut pools = vec![];
+        for asset in astro_assets.0.clone() {
+            pools.push(AstroAsset {
+                info: asset.info.clone(),
+                amount: asset.info.query_pool(&deps.querier, self.contract_addr.to_string())?,
+            })
+        }
+
+        let deposits = [
+            astro_assets
+                .0
+                .iter()
+                .find(|a| a.info.equal(&pools[0].info))
+                .map(|a| a.amount)
+                .expect("Wrong asset info is given"),
+            astro_assets
+                .0
+                .iter()
+                .find(|a| a.info.equal(&pools[1].info))
+                .map(|a| a.amount)
+                .expect("Wrong asset info is given"),
+        ];
+
+        if deposits[0].is_zero() || deposits[1].is_zero() {
+            return Err(StdError::generic_err("Either asset cannot be zero").into());
+        };
+
+        // map over pools
+        for (i, pool) in pools.iter_mut().enumerate() {
+            pool.amount =
+                pool.amount.checked_sub(deposits[i]).map_err(|_| CwDexError::BigIntOverflow {})?;
+        }
+        const MINIMUM_LIQUIDITY_AMOUNT: Uint128 = Uint128::new(1_000);
+        let total_share = self.query_lp_token_supply(&deps.querier)?;
+        let share = if total_share.is_zero() {
+            // Initial share = collateral amount
+            let share = Uint128::new(
+                (U256::from(deposits[0].u128()) * U256::from(deposits[1].u128()))
+                    .integer_sqrt()
+                    .as_u128(),
+            )
+            .saturating_sub(MINIMUM_LIQUIDITY_AMOUNT);
+            // share cannot become zero after minimum liquidity subtraction
+            if share.is_zero() {
+                return Err(StdError::generic_err(
+                    "Share cannot be less than minimum liquidity amount",
+                )
+                .into());
+            }
+            share
+        } else {
+            // Assert slippage tolerance
+            // assert_slippage_tolerance(slippage_tolerance, &deposits, &pools)?;
+
+            // min(1, 2)
+            // 1. sqrt(deposit_0 * exchange_rate_0_to_1 * deposit_0) * (total_share / sqrt(pool_0 * pool_0))
+            // == deposit_0 * total_share / pool_0
+            // 2. sqrt(deposit_1 * exchange_rate_1_to_0 * deposit_1) * (total_share / sqrt(pool_1 * pool_1))
+            // == deposit_1 * total_share / pool_1
+            std::cmp::min(
+                deposits[0].multiply_ratio(total_share, pools[0].amount),
+                deposits[1].multiply_ratio(total_share, pools[1].amount),
+            )
+        };
+        let lp_token = Asset {
+            info: AssetInfoBase::Cw20(Addr::unchecked(self.lp_token_addr.to_string())),
+            amount: share,
+        };
+        Ok(lp_token)
     }
 
     fn simulate_withdraw_liquidity(
@@ -208,12 +288,19 @@ impl Pool for AstroportXykPool {
         deps: Deps,
         offer_asset: Asset,
         ask_asset_info: AssetInfo,
-        minimum_out_amount: Uint128,
-        //For some reason Osmosis requires us to send a sender address for simulation.
-        //This obviously makes no sense and I guess we'll have to make a PR to
-        //Osmosis to fix this, or perhaps copy their math and perform the calculation here...
-        sender: Option<String>,
+        _minimum_out_amount: Uint128, // Astroport doesn't allow setting max spread or belief price on simulated swaps
+        _sender: Option<String>,      // N/A for Astroport
     ) -> StdResult<Uint128> {
-        todo!()
+        let query_msg = QueryMsg::Simulation {
+            offer_asset: cw_asset_to_astro_asset(&offer_asset)?,
+            ask_asset_info: Some(cw_asset_info_to_astro_asset_info(&ask_asset_info)?),
+        };
+        let wasm_query = WasmQuery::Smart {
+            contract_addr: self.contract_addr.to_string(),
+            msg: to_binary(&query_msg)?,
+        };
+        let query_request = QueryRequest::Wasm(wasm_query);
+        let resp = deps.querier.query::<SimulationResponse>(&query_request)?;
+        Ok(resp.return_amount)
     }
 }
