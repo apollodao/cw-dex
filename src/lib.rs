a85227d34c4908e54a19a1786adca6af34609883
Bobby <bobby@apollo.farm>
2022-11-15 15:06:50 +0100

    Add top-level macros and docstring
    Signed-off-by: Bobby <bobby@apollo.farm>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+#![warn(rust_2021_compatibility, future_incompatible, nonstandard_style)]
+#![forbid(unsafe_code)]
+#![deny(bare_trait_objects, unused_doc_comments, unused_import_braces)]
+#![warn(missing_docs)]
+
+//! This is `cw-dex` (wow!)
+
 pub mod error;
 pub mod implementations;
 pub mod traits;
