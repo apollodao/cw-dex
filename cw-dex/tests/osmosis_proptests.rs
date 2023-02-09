6e354bc8e164ab3f0b8814b92a35e7baacb28669
Sturdy <sturdy@apollo.farm>
2023-02-09 15:31:38 +0100

    test: fix prop tests

--- cw-dex/tests/osmosis_proptests.rs
+++ cw-dex/tests/osmosis_proptests.rs
@@ -1,176 +1,105 @@
 use cosmwasm_std::{Coin, Uint128};
 use cw_asset::{Asset, AssetInfo};
 use cw_dex_test_contract::msg::ExecuteMsg;
-use cw_dex_test_helpers::osmosis::{setup_pool_and_test_contract, OsmosisPoolType};
+use cw_dex_test_helpers::osmosis::{setup_pool_and_test_contract, test_pool, OsmosisTestPool};
 use cw_it::helpers::bank_balance_query;
 
 use osmosis_testing::{Module, OsmosisTestApp, RunnerResult, SigningAccount, Wasm};
+use prop::collection::vec;
 use proptest::prelude::*;
 
 const TEST_CONTRACT_WASM_FILE_PATH: &str =
     "../target/wasm32-unknown-unknown/release/osmosis_test_contract.wasm";
 
+const TWO_WEEKS_IN_SECONDS: u64 = 1_209_600;
+
 pub fn setup_pool_and_contract(
-    pool_type: OsmosisPoolType,
-    initial_liquidity: Vec<u64>,
+    pool: &OsmosisTestPool,
 ) -> RunnerResult<(OsmosisTestApp, Vec<SigningAccount>, u64, String)> {
     setup_pool_and_test_contract(
-        pool_type,
-        initial_liquidity,
-        1_209_600, // Two weeks in seconds
+        &pool.pool_type,
+        &pool.pool_liquidity,
+        TWO_WEEKS_IN_SECONDS,
         1,
         TEST_CONTRACT_WASM_FILE_PATH,
     )
 }
 
-fn test_pool_provide_liquidity(
-    pool_type: OsmosisPoolType,
-    initial_liquidity: Vec<u64>,
-    added_liquidity: Vec<u64>,
-) {
-    let (runner, accs, pool_id, contract_addr) =
-        setup_pool_and_contract(pool_type, initial_liquidity.clone()).unwrap();
-
-    // We cannot provide more liquidity than the pool has on Osmosis
-    let added_liquidity: Vec<u64> = added_liquidity
-        .into_iter()
-        .zip(initial_liquidity.into_iter())
-        .map(|(a, b)| a.min(b) - 1u64)
-        .collect();
-
-    let wasm = Wasm::new(&runner);
-    let assets: Vec<Coin> = added_liquidity
-        .into_iter()
-        .enumerate()
-        .map(|(i, amount)| Coin {
-            denom: format!("denom{}", i),
-            amount: amount.into(),
-        })
-        .collect();
-
-    let provide_msg = ExecuteMsg::ProvideLiquidity {
-        assets: assets.clone().into(),
-        min_out: Uint128::one(),
-    };
-    wasm.execute(&contract_addr, &provide_msg, &assets, &accs[0])
-        .unwrap();
-
-    // Query LP token balance
-    let lp_token_balance =
-        bank_balance_query(&runner, contract_addr, format!("gamm/pool/{}", pool_id)).unwrap();
-
-    assert_ne!(lp_token_balance, Uint128::zero());
-}
-
-fn test_pool_swap(
-    pool_type: OsmosisPoolType,
-    pool_liquidity: Vec<u64>,
-    offer: Asset,
-    ask: AssetInfo,
-) {
-    let (runner, accs, _pool_id, contract_addr) =
-        setup_pool_and_contract(pool_type, pool_liquidity).unwrap();
-
-    let wasm = Wasm::new(&runner);
-    let funds = vec![offer.clone().try_into().unwrap()];
-
-    let swap_msg = ExecuteMsg::Swap {
-        offer: offer.clone(),
-        ask: ask.clone(),
-        min_out: Uint128::one(),
-    };
-    wasm.execute(&contract_addr, &swap_msg, &funds, &accs[0])
-        .unwrap();
-
-    // Query LP token balance
-    let offer_balance =
-        bank_balance_query(&runner, contract_addr.clone(), offer.info.to_string()).unwrap();
-    let ask_balance = bank_balance_query(&runner, contract_addr, ask.to_string()).unwrap();
-
-    assert_eq!(offer_balance, Uint128::zero());
-    assert_ne!(ask_balance, Uint128::zero());
-}
-
-fn setup_swap_test_params(
-    pool_liquidity: Vec<u64>,
-    offer_idx: usize,
-    offer_amount: u64,
-    ask_idx: usize,
-    n: usize,
-) -> (Vec<u64>, Asset, AssetInfo) {
-    // Indices mod n
-    let offer_idx = offer_idx % n;
-    let ask_idx = ask_idx % n;
-
-    // If offer and ask are the same, increment ask mod n
-    let ask_idx = if offer_idx == ask_idx {
-        (ask_idx + 1) % n
-    } else {
-        ask_idx
-    };
-
-    // Offer amount cannot be larger than the pool liquidity. This causes the swap
-    // to fail on osmosis.
-    let offer_amount = offer_amount.min(pool_liquidity[offer_idx]);
-
-    // Increment any 0s in the pool liquidity
-    let pool_liquidity: Vec<u64> = pool_liquidity
-        .into_iter()
-        .map(|l| if l == 0 { 1 } else { l })
-        .collect();
-
-    // Construct offer and ask
-    let ask = AssetInfo::Native(format!("denom{}", ask_idx));
-    let offer = Asset {
-        info: AssetInfo::Native(format!("denom{}", offer_idx)),
-        amount: Uint128::from(offer_amount),
-    };
-
-    (pool_liquidity, offer, ask)
-}
-
 proptest! {
     #![proptest_config(ProptestConfig {
-        cases: 16,
+        cases: 64,
         .. ProptestConfig::default()
     })]
 
     #[test]
-    fn test_basic_pool_provide_liquidity(initial_liquidity: [u64; 8], added_liquidity: [u64; 8], n in 2usize..8) {
-        let initial_liquidity = initial_liquidity.into_iter().take(n).collect();
-        let added_liquidity = added_liquidity.into_iter().take(n).collect();
-        test_pool_provide_liquidity(OsmosisPoolType::Basic, initial_liquidity, added_liquidity);
+    fn test_provide_liquidity(
+        (pool,added_liquidity) in test_pool().prop_flat_map(|x| {
+             (Just(x.clone()), vec(1..u64::MAX, x.pool_liquidity.len()))
+        })) {
+        let (runner, accs, pool_id, contract_addr) = setup_pool_and_contract(&pool).unwrap();
+
+        let wasm = Wasm::new(&runner);
+        let assets: Vec<Coin> = added_liquidity
+            .into_iter()
+            .enumerate()
+            .map(|(i, amount)| Coin {
+                denom: format!("denom{}", i),
+                amount: amount.into(),
+            })
+            .collect();
+
+        let provide_msg = ExecuteMsg::ProvideLiquidity {
+            assets: assets.clone().into(),
+            min_out: Uint128::one(),
+        };
+        wasm.execute(&contract_addr, &provide_msg, &assets, &accs[0])
+            .unwrap();
+
+        // Query LP token balance
+        let lp_token_balance =
+            bank_balance_query(&runner, contract_addr, format!("gamm/pool/{}", pool_id)).unwrap();
+
+        assert_ne!(lp_token_balance, Uint128::zero());
     }
 
     #[test]
-    fn test_stable_swap_pool_provide_liquidity(initial_liquidity: [u64; 8], added_liquidity: [u64; 8], scaling_factors: [u32; 8], n in 2usize..8) {
-        let initial_liquidity = initial_liquidity.into_iter().take(n).collect();
-        let added_liquidity = added_liquidity.into_iter().take(n).collect();
-        let scaling_factors = scaling_factors.into_iter().take(n).map(|f| f as u64).collect();
-        test_pool_provide_liquidity(OsmosisPoolType::StableSwap { scaling_factors }, initial_liquidity, added_liquidity);
-    }
+    fn test_pool_swap(
+        (pool,offer_idx,ask_idx, offer_amount) in test_pool().prop_flat_map(|x| {
+            let len = x.pool_liquidity.len();
+            (Just(x.clone()), 0usize..len, 0usize..len)
+        })
+        .prop_filter("Offer and ask can't be the same asset", |(_x, offer_idx, ask_idx)| {
+            offer_idx != ask_idx
+        })
+        .prop_flat_map(|(x, offer_idx, ask_idx)| {
+            (Just(x.clone()), Just(offer_idx), Just(ask_idx), 1..x.pool_liquidity[offer_idx])
+        }),
+    ) {
+        let offer = Asset {
+            info: AssetInfo::Native(format!("denom{}", offer_idx)),
+            amount: Uint128::from(offer_amount),
+        };
+        let ask = AssetInfo::Native(format!("denom{}", ask_idx));
 
-    #[test]
-    fn test_balancer_pool_provide_liquidity(initial_liquidity: [u64; 8], added_liquidity: [u64; 8], pool_weights: [u16; 8], n in 2usize..8) {
-        let initial_liquidity = initial_liquidity.into_iter().take(n).collect();
-        let added_liquidity = added_liquidity.into_iter().take(n).collect();
-        let pool_weights = pool_weights.into_iter().take(n).map(|f| f as u64).collect();
-        test_pool_provide_liquidity(OsmosisPoolType::Balancer { pool_weights }, initial_liquidity, added_liquidity);
-    }
+        let (runner, accs, _pool_id, contract_addr) = setup_pool_and_contract(&pool).unwrap();
 
-    // Works for swap_amount as u64. Fails for u128. Should be fine
-    #[test]
-    fn test_basic_pool_swap(pool_liquidity: [u64; 8], offer_idx in 0usize..7, offer_amount: u64, ask_idx in 0usize..7, n in 2usize..8) {
-        let (pool_liquidity, offer, ask) = setup_swap_test_params(pool_liquidity.into_iter().take(n).collect(), offer_idx, offer_amount, ask_idx, n);
+        let wasm = Wasm::new(&runner);
+        let funds = vec![offer.clone().try_into().unwrap()];
 
-        test_pool_swap(OsmosisPoolType::Basic, pool_liquidity, offer, ask);
-    }
+        let swap_msg = ExecuteMsg::Swap {
+            offer: offer.clone(),
+            ask: ask.clone(),
+            min_out: Uint128::one(),
+        };
+        wasm.execute(&contract_addr, &swap_msg, &funds, &accs[0])
+            .unwrap();
 
-    #[test]
-    fn test_stable_swap_pool_swap(pool_liquidity: [u64; 8], offer_idx in 0usize..7, offer_amount: u64, ask_idx in 0usize..7, n in 2usize..8, scaling_factors: [u32; 8]) {
-        let (pool_liquidity, offer, ask) = setup_swap_test_params(pool_liquidity.into_iter().take(n).collect(), offer_idx, offer_amount, ask_idx, n);
+        // Query LP token balance
+        let offer_balance =
+            bank_balance_query(&runner, contract_addr.clone(), offer.info.to_string()).unwrap();
+        let ask_balance = bank_balance_query(&runner, contract_addr, ask.to_string()).unwrap();
 
-        let scaling_factors = scaling_factors.into_iter().take(n).map(|f| f as u64).collect();
-        test_pool_swap(OsmosisPoolType::StableSwap { scaling_factors }, pool_liquidity, offer, ask);
+        assert_eq!(offer_balance, Uint128::zero());
+        assert_ne!(ask_balance, Uint128::zero());
     }
 }
--- test-helpers/src/osmosis.rs
+++ test-helpers/src/osmosis.rs
@@ -1,5 +1,5 @@
 use cosmwasm_std::{Coin, Uint128};
-use cw_asset::AssetList;
+use cw_asset::{AssetInfo, AssetList};
 use cw_it::helpers::upload_wasm_file;
 use osmosis_std::types::osmosis::gamm::poolmodels::balancer::v1beta1::MsgCreateBalancerPool;
 use osmosis_std::types::osmosis::gamm::poolmodels::stableswap::v1beta1::{
@@ -9,26 +9,79 @@ use osmosis_std::types::osmosis::gamm::v1beta1::{PoolAsset, PoolParams};
 use osmosis_testing::{
     Account, Gamm, Module, OsmosisTestApp, Runner, RunnerResult, SigningAccount,
 };
+use prop::collection::vec;
+use proptest::prelude::{any_with, prop};
+use proptest::prop_compose;
+use proptest::strategy::{Just, Strategy};
+use proptest_derive::Arbitrary;
 
 use crate::instantiate_test_contract;
 
+const MAX_SCALE_FACTOR: u64 = 0x7FFF_FFFF_FFFF_FFFF; // 2^63 - 1
+const MAX_POOL_WEIGHT: u64 = 1048575; //2^20 - 1
+
+#[derive(Debug, Clone, PartialEq, Eq, Arbitrary)]
 pub enum OsmosisPoolType {
     Basic,
-    Balancer { pool_weights: Vec<u64> },
-    StableSwap { scaling_factors: Vec<u64> },
+    Balancer {
+        #[proptest(strategy = "vec(1..MAX_POOL_WEIGHT, param_0.len())")]
+        pool_weights: Vec<u64>,
+    },
+    StableSwap {
+        #[proptest(params = "Vec<u64>")]
+        #[proptest(value = "params.clone()")]
+        scaling_factors: Vec<u64>,
+    },
+}
+
+#[derive(Debug, Clone)]
+pub struct OsmosisTestPool {
+    pub assets: Vec<AssetInfo>,
+    pub pool_liquidity: Vec<u64>,
+    pub pool_type: OsmosisPoolType,
+}
+
+prop_compose! {
+    /// Generates a touple of vectors with (pool_liquidity, scaling_factors) of size 2..8
+    pub fn pool_params()(pool_params in vec((1..u64::MAX, 1..MAX_SCALE_FACTOR), 2..8).prop_filter("scaling factors must be smaller than liquidity",|v| v.iter().all(|(liq, scale)| scale < liq))) -> (Vec<u64>,Vec<u64>) {
+         let (pool_liquidity, scaling_factors): (Vec<u64>,Vec<u64>) = pool_params.into_iter().unzip();
+            (pool_liquidity, scaling_factors)
+    }
+}
+
+prop_compose! {
+    /// Generates a random OsmosisPoolType with the given scaling factors
+    pub fn pool_type(scaling_factors: Vec<u64>)(pool_type in any_with::<OsmosisPoolType>(scaling_factors)) -> OsmosisPoolType {
+        pool_type
+    }
+}
+
+prop_compose! {
+    /// Generates a random OsmosisTestPool with 2..8 assets
+    pub fn test_pool()(pool_params in pool_params())(pool_type in pool_type(pool_params.clone().1), pool_liquidity in Just(pool_params.0)) -> OsmosisTestPool {
+        let mut assets = vec![];
+        for i in 0..pool_liquidity.len() {
+            assets.push(AssetInfo::Native(format!("denom{}", i)));
+        }
+        OsmosisTestPool {
+            assets,
+            pool_liquidity,
+            pool_type,
+        }
+    }
 }
 
 /// Create an Osmosis pool with the given initial liquidity.
 pub fn create_osmosis_pool<'a, R: Runner<'a>>(
     runner: &'a R,
-    pool_type: OsmosisPoolType,
-    initial_liquidity: Vec<Coin>,
+    pool_type: &OsmosisPoolType,
+    initial_liquidity: &[Coin],
     signer: &SigningAccount,
 ) -> u64 {
     let gamm = Gamm::new(runner);
     match pool_type {
         OsmosisPoolType::Basic => {
-            gamm.create_basic_pool(&initial_liquidity, signer)
+            gamm.create_basic_pool(initial_liquidity, signer)
                 .unwrap()
                 .data
                 .pool_id
@@ -70,7 +123,7 @@ pub fn create_osmosis_pool<'a, R: Runner<'a>>(
                         .iter()
                         .map(|c| c.clone().into())
                         .collect(),
-                    scaling_factors,
+                    scaling_factors: scaling_factors.clone(),
                     future_pool_governor: "".to_string(),
                     scaling_factor_controller: "".to_string(),
                 },
@@ -85,8 +138,8 @@ pub fn create_osmosis_pool<'a, R: Runner<'a>>(
 
 /// Setup a pool and test contract for testing.
 pub fn setup_pool_and_test_contract(
-    pool_type: OsmosisPoolType,
-    initial_liquidity: Vec<u64>,
+    pool_type: &OsmosisPoolType,
+    initial_liquidity: &[u64],
     lock_duration: u64,
     lock_id: u64,
     wasm_file_path: &str,
@@ -118,7 +171,7 @@ pub fn setup_pool_and_test_contract(
     let accs = runner.init_accounts(&initial_balances, 10).unwrap();
 
     // Create pool
-    let pool_id = create_osmosis_pool(&runner, pool_type, initial_liquidity, &accs[0]);
+    let pool_id = create_osmosis_pool(&runner, pool_type, &initial_liquidity, &accs[0]);
 
     // Upload test contract wasm file
     let code_id = upload_wasm_file(&runner, &accs[0], wasm_file_path).unwrap();
