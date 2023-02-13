cc23bc48269e825b44194237b4f627bed291259f
Sturdy <sturdy@apollo.farm>
2023-02-13 18:58:19 +0100

    fix: Use correct instantiatemsg in schema

--- test-contracts/astroport-test-contract/src/bin/schema.rs
+++ test-contracts/astroport-test-contract/src/bin/schema.rs
@@ -1,10 +1,10 @@
 use cosmwasm_schema::write_api;
 
-use cw_dex_test_contract::msg::{ExecuteMsg, OsmosisTestContractInstantiateMsg, QueryMsg};
+use cw_dex_test_contract::msg::{AstroportContractInstantiateMsg, ExecuteMsg, QueryMsg};
 
 fn main() {
     write_api! {
-        instantiate: OsmosisTestContractInstantiateMsg,
+        instantiate: AstroportContractInstantiateMsg,
         execute: ExecuteMsg,
         query: QueryMsg,
     }
