d173147cb53b1e716fe11b79654c80f8776a4724
Pacman <pacman@apollo.farm>
2024-08-23 14:51:32 +0200

    refactor!: remove deprecated implementations in cw-dex

--- cw-dex/src/implementations/astroport/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Module containing Pool and Staking implementations for Osmosis
-
-mod pool;
-mod staking;
-
-pub use pool::AstroportPool;
-pub use staking::AstroportStaking;
-
-pub use astroport;
--- cw-dex/src/implementations/osmosis/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Module containing Pool and Staking implementations for Osmosis
-
-mod helpers;
-mod pool;
-mod staking;
-
-pub use pool::*;
-pub use staking::*;
